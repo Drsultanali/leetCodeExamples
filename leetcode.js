77e7204cbefe07cde7ca1b902791e5bc00df5009
Drsultanali <sultan_ali338@yahoo.com>
2024-01-21 14:29:24 +0500

    Compiled the twoSum exmaples ts code to js

--- leetcode.js
+++ leetcode.js
@@ -57,3 +57,21 @@ function climbStairs(n) {
 }
 ;
 console.log(climbStairs(15));
+//Example No-3:
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.*/
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let m = i + 1; m < nums.length; m++) {
+            if (nums[i] + nums[m] === target) {
+                return [i, m];
+            }
+        }
+    }
+    return [];
+}
+;
+let nums = [1, 2, 3, 4, 5];
+let target = 9;
+console.log(twoSum(nums, target));
